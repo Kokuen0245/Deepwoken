1fad7630614a41151208ac6a3eea83d727322158
Kokuen <66192519+Kokuen0245@users.noreply.github.com>
2024-02-24 17:50:25 -0800

    Update AudioPlayer.lua

--- AudioPlayer.lua
+++ AudioPlayer.lua
@@ -1,3 +1,13 @@
+local function sharedRequire(filename)
+    local url = "https://raw.githubusercontent.com/Kokuen0245/Deepwoken/main/" .. filename
+    local response = game:HttpGet(url)
+    if response then
+        return require(response)
+    else
+        error("Error fetching module: " .. filename)
+    end
+end
+
 local Maid = sharedRequire('@utils/Maid.lua');
 local Services = sharedRequire('@utils/Services.lua');
 local library = sharedRequire('@UILibrary.lua');
